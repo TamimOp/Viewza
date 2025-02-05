739d7c903ce41dd710c1d9bba121b4c5a5544fca
TamimOp <tamimshadanik@gmail.com>
2025-02-05 21:15:36 +0600

    connect to mongodb atlas

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants";
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `$(process.env.MONGO_URI)/${DB_NAME}`
+      `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     console.log(
       `\n MongoDb connected !! DB HOST: ${connectionInstance.connection.host}`
