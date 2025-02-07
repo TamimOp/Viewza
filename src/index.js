3aa37ed622737d668223623ecffa0e20c12d9c0a
TamimOp <tamimshadanik@gmail.com>
2025-02-07 23:41:45 +0600

    initialising cors and cookieParser

--- src/index.js
+++ src/index.js
@@ -5,4 +5,13 @@ dotenv.config({
   path: ".env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port: ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDb Connection ERROR: ", error);
+    process.exit(1);
+  });
