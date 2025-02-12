95af1d16a54ac24634efc6ec9672e81f4c6427a2
TamimOp <tamimshadanik@gmail.com>
2025-02-12 23:14:41 +0600

    using hooks to hash password before saving, compared passaword, access token, refresh token generate, jwt, bcrypt implement

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -47,4 +49,40 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
+/// using hooks to hash password before saving
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+///compare password
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+///access token generate
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_SECRET_EXPIRY }
+  );
+};
+///Refresh token generate
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_SECRET_EXPIRY }
+  );
+};
 export const User = mongoose.model("User", userSchema);
