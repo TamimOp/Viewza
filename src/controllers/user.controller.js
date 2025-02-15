0f2e5af2756711f56192be16a2d586d604583a6f
TamimOp <tamimshadanik@gmail.com>
2025-02-15 23:56:48 +0600

    async handler fix,user route created

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,3 +3,5 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({ success: true, message: "User registered" });
 });
+
+export { registerUser };
